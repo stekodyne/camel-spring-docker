ca68332716846a46719cb6e2a32326d8cb7cd009
Steffen Kory <Steffen.Kory@csra.com>
2017-02-23 08:27:12 -0800

    Configured Docker image to install cert.

--- Docker/build.sh
+++ Docker/build.sh
@@ -5,7 +5,7 @@ DOCKER_IMAGE_VERSION=latest
 mvn clean package
 
 docker rmi --force=true ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
-docker build --force-rm=true -f Docker/Dockerfile --rm=true -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
+docker build --no-cache=true --force-rm=true -f Docker/Dockerfile --rm=true -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
 echo =========================================================================
 echo Docker image is ready.  Try it out by running:
 echo     docker run --rm -ti -p 8080:8080 -v local_directory:/tmp -P ${DOCKER_IMAGE_NAME}
--- Docker/install.sh
+++ Docker/install.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+set -e
+
+openssl s_client -connect 10.255.242.20:143 -starttls imap > imap.cer
+
+cat imap.cer | awk '/-----BEGIN CERTIFICATE-----/{f=1} /-----END CERTIFICATE-----/{f=0;print} f' > imap.cert
+
+echo "yes" | $JAVA_HOME/bin/keytool -import -alias soa1 -file imap.cert -keystore $JAVA_HOME/jre/lib/security/jssecacerts -storepass changeme
+
+rm imap.*
+rm install.sh
